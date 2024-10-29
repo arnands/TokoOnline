9376a85d453f58f5675f0b3f2a6b429fe32935c1
arnands <arnandasr@gmail.com>
2024-10-29 12:18:19 +0700

    first

---
+++ resources/views/backend/v_layouts/app-old.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Toko Online</title>
+</head>
+
+<body>
+    <a href="{{ route('backend.beranda') }}">Beranda</a>
+    <a href="#">user</a>
+    <a href="#" onclick="event.preventDefault(); document.getElementById('keluar-app').submit();">keluar</a>
+
+    <p></p>
+    <!-- @yieldawal -->
+    @yield('content')
+    <!-- @yieldakhir -->
+
+    <!-- Keluar App -->
+    <form action="{{ route('backend.logout') }}" method="POST" id="keluar-app" class="d-none">
+    @csrf
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda')->middleware('auth');
+
+Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
+Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
+Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
+
+Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
